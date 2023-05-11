e2086eb94055150cb9105d545c38b208f9c29c7a
HwangJaeHyeong <ghkdwogud852@gmail.com>
2023-05-11 16:26:44 +0900

    feat : TodoItem 컴포넌트 기능 추가

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,6 +1,36 @@
 import React from "react";
-import { Root } from "./styled";
+import { DeleteButton, Root, StyledCheckbox, TodoTypo } from "./styled";
+import { useNavigate } from "react-router-dom";
 
-export const TodoItem = () => {
-  return <Root>TodoItem</Root>;
+/**
+ * @prop id: TodoItem id(pk)
+ * @prop isChecked: TodoItem 토글 여부(boolean)
+ * @prop onToggleTodoItem: TodoItem 토글시 호출하는 function
+ * @prop todoValue: todoItem value(string)
+ * @prop onDeleteTodoItem: TodoItem 삭제시 호출하는 function
+ */
+export const TodoItem = ({
+  id,
+  isChecked,
+  onToggleTodoItem,
+  todoValue,
+  onDeleteTodoItem,
+}) => {
+  const navigate = useNavigate();
+
+  const onClickTodoTypo = () => {
+    navigate(`/details/${id}`);
+  };
+
+  return (
+    <Root>
+      <StyledCheckbox
+        value={isChecked}
+        onClick={onToggleTodoItem}
+        type="checkbox"
+      />
+      <TodoTypo onClick={onClickTodoTypo}>{todoValue}</TodoTypo>
+      <DeleteButton onClick={onDeleteTodoItem}>삭제</DeleteButton>
+    </Root>
+  );
 };
--- src/components/TodoItem/styled.js
+++ src/components/TodoItem/styled.js
@@ -1,3 +1,13 @@
 import styled from "styled-components";
 
-export const Root = styled.div``
+export const Root = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 5px;
+`;
+
+export const StyledCheckbox = styled.input``;
+
+export const TodoTypo = styled.p``;
+
+export const DeleteButton = styled.button``;
