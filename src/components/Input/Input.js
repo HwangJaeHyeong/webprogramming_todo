077c5d1406b5ebe6172e452473eb1562993e4cf9
HwangJaeHyeong <ghkdwogud852@gmail.com>
2023-05-11 16:11:13 +0900

    initialize

---
+++ src/components/Input/Input.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Root } from './styled'
+
+export const Input = () => {
+  return <Root />
+}
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Input } from "../../components/Input";
+import { TodoItem } from "../../components/TodoItem";
+import { Root } from "./styled";
+
+let count = 1;
+
+export const MainPage = () => {
+  const [todoListData, setTodoListData] = useState([{ id: 0, title: "투두1" }]);
+  const [inputValue, setInputValue] = useState(0);
+  const onChangeInputValue = (e) => {
+    setInputValue(e.target.value);
+  };
+  const onCreateTodoItem = () => {
+    setTodoListData((prev) => prev.concat({ id: count, title: inputValue }));
+    setInputValue("");
+
+    count++;
+  };
+  return (
+    <Root>
+      <Input
+        inputValue={inputValue}
+        onChangeInputValue={onChangeInputValue}
+        onCreateTodoItem={onCreateTodoItem}
+      />
+      {todoListData.map((todoItemData) => (
+        <TodoItem
+          title={todoItemData.title}
+          key={`todo_item_${todoItemData.id}`}
+        />
+      ))}
+    </Root>
+  );
+};
