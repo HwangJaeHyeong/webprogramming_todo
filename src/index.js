93d4df40a3c50568ada634ae9b1715da45cc2401
HwangJaeHyeong <ghkdwogud852@gmail.com>
2023-05-11 16:14:55 +0900

    feat : Router 모듈 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { MainPage } from "./pages/Main";
+import { DetailsPage } from "./pages/Details";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "/details/:todoId",
+    element: <DetailsPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MainPage />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
